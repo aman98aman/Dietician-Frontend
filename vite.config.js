c3566151ee83e000d3a01a84d086cd4e4a9c7c0a
Aman Kapoor <kapooraman32@gmail.com>
2024-09-14 15:56:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default {
-  server: {
-    host: '0.0.0.0',  // Bind to all IP addresses
-    port: 5173,       // The default port
-    strictPort: true  // Fail if port 5173 is in use
-  }
-}
-
+export default defineConfig({
+  plugins: [react()],
+});
