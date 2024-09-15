f7ace72ebd5c521656dedc6b6311380813348611
Aman Kapoor <kapooraman32@gmail.com>
2024-09-15 16:57:30 +0530

    Update AxiosInterceptor.js

--- src/components/AxiosInterceptor.js
+++ src/components/AxiosInterceptor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://fitness.arshsandhufitness.com/",
+  baseURL: "https://dietbackend.azurewebsites.net/",
   timeout: 10000,
 });
 
